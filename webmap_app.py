cad88f779bfc55dbc6e30ad0d2d51fea9b695154
Farhan Ali <farhanaliw6@gmail.com>
2018-08-10 15:54:47 +0100

    Volcano and Population layers added. Users can toggle them.

--- webmap_app.py
+++ webmap_app.py
@@ -17,13 +17,21 @@ def colour_producer(elevation):
 
 my_map = folium.Map(location = [38.58, -99.09], zoom_start = 6, tiles = "Mapbox Bright")
 
-fg = folium.FeatureGroup(name = "My Map")
+fgv = folium.FeatureGroup(name = "Volcanoes")
 
 for lat, lon, elev in zip(latitude, longitude, elevation):
-    fg.add_child(folium.CircleMarker(location = [lat, lon], radius = 6,
+    fgv.add_child(folium.CircleMarker(location = [lat, lon], radius = 6,
     popup = str(elev) + " m", fill_color = colour_producer(elev), color = 'grey', fill_opacity = 0.7,
     fill = True))
-    
-my_map.add_child(fg)
 
+fgp = folium.FeatureGroup(name = "Population")
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), 
+            style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+            if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+
+my_map.add_child(fgv)
+my_map.add_child(fgp)
+my_map.add_child(folium.LayerControl())
 my_map.save("my_map.html")
